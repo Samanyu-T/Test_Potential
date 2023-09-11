74d942c3eefffdf5744f7b244fa4ab9f3941811b
Samanyu-T <samanyut8@gmail.com>
2023-09-11 15:43:08 +0100

    'Update'

--- Python Scripts/Point_Defects.py
+++ Python Scripts/Point_Defects.py
@@ -105,22 +105,51 @@ class Lammps_Point_Defect():
 
         lmp.command('mass 3 4.002602')
 
+        mass = 1.03499e-4* np.array([183.84, 10.00784, 4.002602])
+
         lmp.command('pair_style eam/alloy' )
 
         lmp.command('pair_coeff * * %s W H He' % self.potfile)
 
-        for i in range(10):
-            if len(xyz_inter[1]) > 1:
+        N_anneal = 2
 
-                lmp.command('group h_int type 2')
+        E0 = 2
 
-                lmp.command('velocity h_int create %f %d dist gaussian mom yes rot no units box' % (100.0, rng_num) )
-            
-            elif len(xyz_inter[1]) == 1:
+        EN = 1
+
+        constant = (2/(3*8.617333262e-5))
+
+        T0 = constant*E0
+
+        TN = constant*EN
+
+        decay_constant = (1/N_anneal)*np.log(T0/TN)
+
+        for i in range(N_anneal):
+
+            for element in range(len(xyz_inter)):
                 
-                lmp.command('group h_int type 2')
+                lmp.command('group int_%d type %d' % (element+1, element+1))
 
-                lmp.command('velocity h_int set %f %f %f sum yes units box' % (0.1, 0.1 , 0.1))
+                if len(xyz_inter[element]) > 1:
+
+                    T = T0*np.exp(-decay_constant*i)
+
+                    lmp.command('velocity int_%d create %f %d dist gaussian mom yes rot no units box' % (element+1 ,T, rng_num) )
+                
+                elif len(xyz_inter[element]) == 1:
+
+                    E = E0*np.exp(-decay_constant*i)
+
+                    speed = np.sqrt(2*E/mass[element])
+
+                    unit_vel = np.hstack( [np.random.randn(1), np.random.randn(1), np.random.randn(1)] )
+
+                    unit_vel /= np.linalg.norm(unit_vel)
+
+                    vel = speed*unit_vel
+
+                    lmp.command('velocity int_%d set %f %f %f sum yes units box' % (element+1,vel[0], vel[1], vel[2]))
 
             lmp.command('run 0')
 
@@ -130,7 +159,7 @@ class Lammps_Point_Defect():
 
             lmp.command('thermo_style custom step temp pe press') 
 
-            lmp.command('run 200')
+            lmp.command('run 100')
 
             lmp.command('minimize 1e-5 1e-8 10 10')
 
@@ -232,6 +261,8 @@ class Lammps_Point_Defect():
 
 def optimize_sites(Instance):
 
+    #Find the sites of interest in a BCC crystal with a vacancy
+
     oct =  size//2 + Instance.get_octahedral_sites()
 
     tet = size//2 + Instance.get_tetrahedral_sites()
@@ -250,50 +281,54 @@ def optimize_sites(Instance):
 
     energies = []
 
-    for i in range(6):
+    for n_vac in range(1,2):
 
-        new_site_idx = [0, 0, 0, 0]
+        Instance.n_vac = n_vac
 
-        new_site_pe = np.zeros(shape=(len(available_sites) ,))
+        for n_h in range(10):
 
-        for site_type_idx in range(len(available_sites)):
+            new_site_idx = [0, 0, 0, 0]
 
-            distance = np.zeros(shape= (len(available_sites[site_type_idx]),) )
-            test_sites = []
+            new_site_pe = np.zeros(shape=(len(available_sites) ,))
 
-            for prev_site_idx in range(len(previous_sites[1])):
+            for site_type_idx in range(len(available_sites)):
 
-                prev_site_element = np.array(previous_sites[1][prev_site_idx])
+                if len(available_sites[site_type_idx]) > 0:
 
-                distance += np.linalg.norm(available_sites[site_type_idx] - prev_site_element, axis = 1)
-            
-            new_site_idx[site_type_idx] = int(np.argmax(distance))
+                    distance = np.zeros(shape= (len(available_sites[site_type_idx]),) )
+                    test_sites = []
 
-            test_sites.append(copy.deepcopy(previous_sites))
+                    for prev_site_idx in range(len(previous_sites[1])):
 
-            test_sites[0][1].append(available_sites[site_type_idx][new_site_idx[site_type_idx]])
+                        prev_site_element = np.array(previous_sites[1][prev_site_idx])
 
-            new_site_pe[site_type_idx] = Instance.Build_Defect(test_sites[0])
+                        distance += np.linalg.norm(available_sites[site_type_idx] - prev_site_element, axis = 1)
+                    
+                    new_site_idx[site_type_idx] = int(np.argmax(distance))
 
-        min_pe_idx = np.argmin(new_site_pe)
+                    test_sites.append(copy.deepcopy(previous_sites))
 
-        previous_sites[1].append(available_sites[min_pe_idx][new_site_idx[min_pe_idx]])
+                    test_sites[0][1].append(available_sites[site_type_idx][new_site_idx[site_type_idx]])
 
-        available_sites[min_pe_idx] = np.delete(available_sites[min_pe_idx], new_site_idx[min_pe_idx], axis= 0)
+                    new_site_pe[site_type_idx] = Instance.Build_Defect(test_sites[0])
+                
+                else:
 
-        store_sites.append(copy.deepcopy(previous_sites))
+                    new_site_pe[site_type_idx] = np.inf
 
-        energies.append(new_site_pe[min_pe_idx])
+            min_pe_idx = np.argmin(new_site_pe)
 
-    return energies
+            previous_sites[1].append(available_sites[min_pe_idx][new_site_idx[min_pe_idx]])
+
+            available_sites[min_pe_idx] = np.delete(available_sites[min_pe_idx], new_site_idx[min_pe_idx], axis= 0)
+
+            store_sites.append(copy.deepcopy(previous_sites))
+
+            energies.append(new_site_pe[min_pe_idx])
+
+    return store_sites, energies
 
 
-
-        
-
-
-
- 
 size = 7
 
 b_energy = np.array([-8.949, -4.25/2, 0])
@@ -333,11 +368,14 @@ defect = Instance.Build_Defect(intersitial_sites)
 
 N_atoms = 2*size**3
 
+sites, defect_energies = optimize_sites(Instance)
+
 if Instance.me == 0:
     #print(defect -  perfect*((N_atoms-1)/N_atoms) - np.dot(Instance.n_inter  ,b_energy))
     print(defect - vacancy - h_int + perfect)
     print('H + Vacancy',defect, 'H Intersitial' , h_int,'Vacancy' ,vacancy,'Perfect', perfect,'Gas in Vacuum', np.dot(Instance.n_inter  ,b_energy))
     print('Vacancy',vacancy - perfect - 8.949)
     print('H-Intersitial',h_int - perfect - np.dot(Instance.n_inter  ,b_energy))
+    print(sites)
+    print(defect_energies)
 
-print(optimize_sites(Instance))
